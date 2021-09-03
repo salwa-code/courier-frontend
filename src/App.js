a054eea527c9dcdbdf254ecd602bdf1d84c0693f
Tuhirirwe-Maria <tuhirirwemaria@gmail.com>
2021-09-03 07:27:54 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Components/Header';
+import Section from './Components/Section';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Section/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <>
+        <nav className = "navbar">
+            <div className = "logo">
+                <h1>Safe Courier</h1>
+            </div>
+
+            <div className = "nav-icons">
+            <i class="fas fa-envelope-open-text"></i>
+            <i class="fas fa-bell"></i>
+            <i class="fas fa-user-circle"></i>
+
+            </div>
+         
+        </nav>
+        <img className = "image" src="https://res.cloudinary.com/awesomeone-maria/image/upload/v1630576323/capstone%20Project/undraw_takeout_boxes_ap54_xamxen.svg" alt="Delivery" />
+        </>
+    )
+}
+
+export default Header
