dfe27b017ac2f49bdadf5118521b8380b79222e6
Philari <arindap4@gmail.com>
2021-09-04 21:43:52 +0300

    Sign in first commit

---
+++ src/Components/FormSuccess.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Components/Form.css';
+
+const FormSuccess = () => {
+  return (
+    <div className='form-content-right'>
+      <h1 className='form-success'>Sign in Successful</h1>
+    </div>
+  );
+};
+
+export default FormSuccess; 
